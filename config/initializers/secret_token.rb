ed46590f02d7550930f470bcb44d9362b5c490bf
startae14 <startae14@gmail.com>
2014-04-09 00:59:06 +0200

    Primo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrimaApp::Application.config.secret_token = '5fd5b15943e9f0a8c78a138d1ddfedbf4ae761c30b866086053826f6ed9a94af484d7a078c8064c8e634390111f774cc840bb755e59e51bd9539b35d5f19dc6c'
